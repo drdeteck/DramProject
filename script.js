67677cde837bd0ee932c6f7a93d21b1d0a5ee8a8
Philippe Lavoie <drdeteck@gmail.com>
2012-09-27 23:34:30 -0400

    Working with bootstrap

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ $(document).ready(function()
 	$('#balvenie-ra').roundabout();
 	$('#glencairn-ra').roundabout();
 
-	$('#nav a').click(function() {
-		pageId = $(this).attr('href');
-		num = $('#nav a').index(this);
+	$('#paging a').click(function() {
+		pageId = $(this).attr('page');
+		num = $('#paging a').index(this);
 		$(pageId).parent().animate({scrollTop: (700 * num)}, 'slow');
+		$("#paging li").removeClass("active");
+		$(this).parent().addClass("active");
 	});
 });
 
