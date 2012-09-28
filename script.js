c313f9002b6cedac762e7eac1643785777b3a620
Philippe Lavoie <drdeteck@gmail.com>
2012-09-28 09:20:31 -0400

    fine tuning of syle

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
 
 $(document).ready(function()
 {
-	$('#balvenie-ra').roundabout();
+	// Settings sizes
+	var docHeight = $(document).height()
+	var over = docHeight - $(".page").first().height();
+	var padding = over / 2;
+
+	$("#wrapper").height(docHeight - padding);
+	$("#wrapper").css("padding-top", padding);
+	$(".page").css("padding-bottom", padding);
+
+	$('#balvenie-ra').roundabout(
+	{
+		responsive:true
+	});
 	$('#glencairn-ra').roundabout();
 
 	$('#paging a').click(function() {
 		pageId = $(this).attr('page');
 		num = $('#paging a').index(this);
-		$(pageId).parent().animate({scrollTop: (700 * num)}, 'slow');
+		$(pageId).parent().animate({scrollTop: (docHeight * num)}, 'slow');
 		$("#paging li").removeClass("active");
 		$(this).parent().addClass("active");
 	});
+
+
+
+	var feedUrl = "http://www.google.com/calendar/feeds/liz@gmail.com/public/full";
+	feedUrl = "https://docs.google.com/a/philippelavoie.com/spreadsheet/ccc?key=0AoKnDojyuN8YdGZVaGpoQmhhOE5PbU1pcGRVWFctcUE#gid=0";
+
+	function setupMyService() {
+		var myService = new google.gdata.calendar.CalendarService('exampleCo-exampleApp-1');
+		return myService;
+	}
+
+	function getMyFeed() {
+		myService = setupMyService();
+
+		myService.getEventsFeed(feedUrl, handleMyFeed, handleError);
+	}
 });
 
